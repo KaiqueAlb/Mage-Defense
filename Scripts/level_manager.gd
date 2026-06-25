a4bc26fa8c4369c5d4d19a89b7ae877d8e85f432
Rhapsody <flaviofilhoag@gmail.com>
2026-06-25 14:11:24 -0300

    organizacao de arquivos e comeco de selecionar torres

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -3,8 +3,13 @@ extends Node
 @export var selected_tower_scene: PackedScene
 @export var selected_tower_status: StatusTorre
 
+var choose_tower = true;
+
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
+	if choose_tower:
+		
+		pass
 	pass
